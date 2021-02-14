3b41cddc00867c728598c8ffb2f9710f8974ff76
Mathric <mathric2015@gmail.com>
2021-02-15 03:40:39 +0800

    Add function track current tab id

--- randomplay.js
+++ randomplay.js
@@ -1,3 +1,8 @@
+let curTabId = null
+let curTabId2 = null
+let newTabId = null
+let tabFlg = true
+
 function searchFolder(bookmarks, target) {
   for(let i = 0; i < bookmarks.length; i++) {
     let bookmark = bookmarks[i]
@@ -17,3 +22,16 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     searchFolder(TreeNodes, "music")
   })
 })
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.get(activeInfo.tabId, function (tab) {
+    if(tabFlg) {
+      curTabId = tab.id
+      tabFlg = false
+    }
+    else {
+      curTabId2 = tab.id
+      tabFlg = true
+    }      
+  });
+});
