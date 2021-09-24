b21b648983dd1be5df829e29b825d7b2c366b335
mathric <mathric2015@gmail.com>
2021-09-24 10:22:05 -0400

    Change the structure and the way to append child The structure now would be like following: <ul>     <li>title</li>     <li>directory         <ul></ul>     </li> </ul>

--- folder_option.js
+++ folder_option.js
@@ -1,4 +1,4 @@
-let root_folder = document.getElementById("root_folder");
+let root_folder = document.getElementById("root_folder_list");
 let directoryUnfoldState = new Map();
 
 chrome.bookmarks.getTree(function (TreeNodes) {
@@ -8,31 +8,21 @@ chrome.bookmarks.getTree(function (TreeNodes) {
 function createDynamicList(node, htmlParent) {
     //check if it is directory
     if (node.hasOwnProperty("children")) {
-        let child_list = document.createElement("ul");
         for (let i = 0; i < node.children.length; i++) {
-            let child = null
+            let child = document.createElement("li");
+            child.textContent = node.children[i].title;
 
             if (node.children[i].hasOwnProperty("children")) {
-                child = document.createElement("ul");
+                let grandChild = document.createElement("ul");
+                child.appendChild(grandChild)
                 child.addEventListener("click", function (e) {
-                    // let cur_unfold_state = directoryUnfoldState[node.children[i].id];
-
-                    // if (cur_unfold_state === undefined || !cur_unfold_state) {
-                    //     createDynamicList(node.children[i], child);
-                    //     directoryUnfoldState[node.children[i].id] = true;
-                    // }
-                    foldClickHandler(node.children[i], child)
-
+                    foldClickHandler(node.children[i], grandChild)
                     e.stopPropagation();
                 });
             }
-            else {
-                child = document.createElement("li");
-            }
-            child.textContent = node.children[i].title;
-            child_list.appendChild(child);
+            
+            htmlParent.appendChild(child);  
         }
-        htmlParent.appendChild(child_list);
     }
 }
 
@@ -45,11 +35,12 @@ function foldClickHandler(node, curElement) {
         directoryUnfoldState[node.id] = true;
     }
     else{
-        //fold it(remove all child)
+        //fold it(remove all grandchild)
         console.log(curElement)
         while (curElement.firstChild) {
-            console.log(curElement.firstChild)
+            //console.log(curElement.firstChild)
             curElement.removeChild(curElement.lastChild);
         }
+        directoryUnfoldState[node.id] = false;
     }
 }
