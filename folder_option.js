15b0dab214398e13cf09617bdbe92ba1e85fec55
mathric <mathric2015@gmail.com>
2021-09-24 09:50:13 -0400

    Seperate the click function in creat list function and add fold logic in it

--- folder_option.js
+++ folder_option.js
@@ -1,34 +1,55 @@
-let root_folder = document.getElementById("root_folder")
-let directoryUnfoldState = new Map()
+let root_folder = document.getElementById("root_folder");
+let directoryUnfoldState = new Map();
 
 chrome.bookmarks.getTree(function (TreeNodes) {
-    createDynamicList(TreeNodes[0], root_folder)
-})
-
+    createDynamicList(TreeNodes[0], root_folder);
+});
 
 function createDynamicList(node, htmlParent) {
     //check if it is directory
-    if(node.hasOwnProperty("children")) {
-        let child_list = document.createElement("ul")
-        for(let i = 0; i < node.children.length; i++) {
-            let child = document.createElement("li")
-            child.textContent =  node.children[i].title
+    if (node.hasOwnProperty("children")) {
+        let child_list = document.createElement("ul");
+        for (let i = 0; i < node.children.length; i++) {
+            let child = null
 
-            if(node.children[i].hasOwnProperty("children")) {
-                child.addEventListener("click", function(e) {
-                    let cur_unfold_state = directoryUnfoldState[node.children[i].id]
+            if (node.children[i].hasOwnProperty("children")) {
+                child = document.createElement("ul");
+                child.addEventListener("click", function (e) {
+                    // let cur_unfold_state = directoryUnfoldState[node.children[i].id];
 
-                    if(cur_unfold_state === undefined || !cur_unfold_state){
-                        createDynamicList(node.children[i], child)
-                        directoryUnfoldState[node.children[i].id] = true
-                    }
-                    
+                    // if (cur_unfold_state === undefined || !cur_unfold_state) {
+                    //     createDynamicList(node.children[i], child);
+                    //     directoryUnfoldState[node.children[i].id] = true;
+                    // }
+                    foldClickHandler(node.children[i], child)
+
                     e.stopPropagation();
-                })
+                });
             }
-            child_list.appendChild(child)
-            
+            else {
+                child = document.createElement("li");
+            }
+            child.textContent = node.children[i].title;
+            child_list.appendChild(child);
         }
-        htmlParent.appendChild(child_list)
+        htmlParent.appendChild(child_list);
+    }
+}
+
+function foldClickHandler(node, curElement) {
+    let cur_unfold_state = directoryUnfoldState[node.id];
+
+    //unfold it 
+    if (cur_unfold_state === undefined || !cur_unfold_state) {
+        createDynamicList(node, curElement);
+        directoryUnfoldState[node.id] = true;
+    }
+    else{
+        //fold it(remove all child)
+        console.log(curElement)
+        while (curElement.firstChild) {
+            console.log(curElement.firstChild)
+            curElement.removeChild(curElement.lastChild);
+        }
     }
 }
